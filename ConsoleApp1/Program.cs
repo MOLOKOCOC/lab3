572fb9f0c9e78816bf6c39ba7c47689ab66bcd36
MOLOKOCOC <mmm.mch@mail.ru>
2022-02-09 14:04:44 +0500

    C:/Program Files/Git/

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            else if (c == 4) { Console.WriteLine(a / b); }
+
+        }
+    }
+}
