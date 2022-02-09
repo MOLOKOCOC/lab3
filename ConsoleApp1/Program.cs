45207aae06451b439b42c3e030607bd3a2cdffd9
MOLOKOCOC <mmm.mch@mail.ru>
2022-02-09 14:03:29 +0500

    *

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            else if (c == 3) { Console.WriteLine(a * b); }
+
+        }
+    }
+}
