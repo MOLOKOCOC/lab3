7def72af66e839a81035d945b5605cce6bacd719
MOLOKOCOC <mmm.mch@mail.ru>
2022-02-09 13:44:14 +0500

    labA

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 0, c = 0;
+
+            Console.WriteLine("введите а \n введите b \n 1) + \n 2) - \n 3) * \n 4) /");
+
+            a = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
