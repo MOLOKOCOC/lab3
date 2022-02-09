f7c880360b52627518a957df8d0d4df95d52a58d
MOLOKOCOC <mmm.mch@mail.ru>
2022-02-09 12:58:54 +0500

    lab2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,12 +1,1 @@
-﻿using System;
-
-namespace ConsoleApp1
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+﻿
