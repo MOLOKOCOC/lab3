42545743eec3ce47fcb4773050c9dc71aa336b19
MOLOKOCOC <mmm.mch@mail.ru>
2022-02-09 14:00:06 +0500

    +

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            c = Convert.ToInt32(Console.ReadLine());
+            if (c == 1) { Console.WriteLine(a + b); }
+        }
+    }
+}
